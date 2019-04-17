cc85cb4638b7f4ed7d3168a7e8cdb73c1f0c5d75
obe <obezzanou@gmail.com>
2019-04-17 10:56:47 +0200

    cache management

--- src/main/java/org/apache/oozie/event/listener/monitoring/MonitoringListener.java
+++ src/main/java/org/apache/oozie/event/listener/monitoring/MonitoringListener.java
@@ -33,37 +33,40 @@ public class MonitoringListener extends JobEventListener {
 	private static final String KAFK_BOOTSTRAP_SERVERS = "oozie.job.listener.kafka.bootstrap.servers";
 	private static final String KAFKA_TOPIC = "oozie.job.listener.kafka.topic";
 
+	private final Cache<String, KafkaProducer<String, String>> cache = CacheBuilder.newBuilder()
+			.expireAfterAccess(5, TimeUnit.MINUTES)
+			.removalListener((RemovalListener<String, KafkaProducer<String, String>>) notification -> {
+				notification.getValue().close();
+				LOGGER.info("Close unused producer");
+			}).build();
+	
 	private Gson mapper = new Gson().newBuilder().setPrettyPrinting()
 			.registerTypeAdapter(Date.class, new DateTypeAdapter()).create();
 
 	private String kafkaBootstrapServers;
 	private String kafkaTopic;
-	
-	private static final Cache<String, KafkaProducer<String, String>> CACHE;
 
-	static {
-		CACHE = CacheBuilder.newBuilder().expireAfterAccess(5, TimeUnit.MINUTES)
-				.removalListener((RemovalListener<String, KafkaProducer<String, String>>) notification -> notification
-						.getValue().close())
-				.build();
-	}
-
-	public synchronized KafkaProducer<String, String> getProducer() {
-		if(StringUtils.isEmpty(kafkaBootstrapServers) || StringUtils.isEmpty(kafkaTopic)) {
+	public KafkaProducer<String, String> getProducer() {
+		if (StringUtils.isEmpty(kafkaBootstrapServers) || StringUtils.isEmpty(kafkaTopic)) {
 			LOGGER.error(KAFK_BOOTSTRAP_SERVERS + " / " + KAFKA_TOPIC + " are empty");
 			return null;
 		}
-		KafkaProducer<String, String> kafkaProducer = CACHE.getIfPresent(SINGLE_PRODUCER);
-		if(kafkaProducer == null) {
+		KafkaProducer<String, String> kafkaProducer = cache.getIfPresent(SINGLE_PRODUCER);
+		if (kafkaProducer == null) {
 			Properties properties = new Properties();
 			properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
 			properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 			properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 			properties.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 1000);
 			kafkaProducer = new KafkaProducer<>(properties);
-			CACHE.put(SINGLE_PRODUCER, kafkaProducer);
+			cache.put(SINGLE_PRODUCER, kafkaProducer);
 		}
 		return kafkaProducer;
+	}
+
+	public void closeProducer() {
+		cache.invalidateAll();
+		LOGGER.warn("Producer closed");
 	}
 
 	@Override
@@ -80,6 +83,7 @@ public class MonitoringListener extends JobEventListener {
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.debug("Monitoring listener destroyed");
 		}
+		closeProducer();
 	}
 
 	@Override
@@ -107,11 +111,13 @@ public class MonitoringListener extends JobEventListener {
 		sendEventToKafka(eventToJson(bje));
 	}
 
-	private void sendEventToKafka(String event) {
+	private synchronized void sendEventToKafka(String event) {
+		KafkaProducer<String, String> producer = null;
 		try {
-			KafkaProducer<String, String> producer = getProducer();
-			if(producer != null) {
-				Future<RecordMetadata> futureSend = producer.send(new ProducerRecord<String, String>(kafkaTopic, event));
+			producer = getProducer();
+			if (producer != null) {
+				Future<RecordMetadata> futureSend = producer
+						.send(new ProducerRecord<String, String>(kafkaTopic, event));
 				RecordMetadata metadata = futureSend.get();
 				if (LOGGER.isDebugEnabled()) {
 					LOGGER.debug("[" + metadata + "] is sent");
@@ -119,6 +125,7 @@ public class MonitoringListener extends JobEventListener {
 			}
 		} catch (Exception th) {
 			LOGGER.error("Error sending event (" + event + ") to kafka", th);
+			closeProducer();
 		}
 	}
 
